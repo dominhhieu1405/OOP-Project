b076131013368176e114ea0ce9f4a20bfac0b8f4
Nguyen-Duc-123 <24021415@vnu.edu.vn>
2025-10-01 09:34:09 +0700

    Update Breakable interface and Brick class

--- src/entities/Brick.java
+++ src/entities/Brick.java
@@ -1,4 +1,30 @@
 package entities;
-public class Brick extends Entity implements Breakable {
 
+public class Brick extends Entity implements Breakable {
+    private int hitPoint;
+    private boolean destroyed;
+    private double powerUpDropChance;
+    private int scoreValue;
+
+    @Override
+    public boolean hit();
+
+    @Override
+    public boolean isDestroyed();
+
+    /**
+     * Xử lí khi bị hit.
+     */
+    public void onHit();
+
+    /**
+     * Xử lí khi bị phá hủy (Bao nhiêu điểm , có rơi ra vật phẩm gì không , âm thanh
+     * khi bị phá hủy)
+     */
+    public void onDestroyed();
+
+    /**
+     * Xử lí va chạm với bóng
+     */
+    public boolean checkCollisionWithBall(Ball ball);
 }
