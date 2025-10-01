c2ac816331ebb30512e69425245bf829b4755755
HuyPenguinn <24021519@vnu.edu.vn>
2025-10-01 10:58:15 +0700

    update(constants)

--- src/entities/Ball.java
+++ src/entities/Ball.java
@@ -1,28 +1,4 @@
 package entities;
+import utils.Constants;
 public class Ball extends Entity implements Movable {
-    private int velocityX;
-    private int velocityY;
-    private int x;
-    private int y;
-    public void move() {
-        this.x += velocityX;
-        this.y += velocityY;
-    }
-    public void setVelocity(int velocityX, int velocityY) {
-        this.velocityX = velocityX;
-        this.velocityY = velocityY;
-    }
-    public int getVelocityX() {
-        return this.velocityX;
-    }
-    public int getVelocityY() {
-        return this.velocityY;
-    }
-    public int getX() {
-        return this.x;
-    }
-    public int getY() {
-        return this.y;
-    }
-
 }
--- src/entities/Movable.java
+++ src/entities/Movable.java
@@ -1,7 +1,3 @@
 package entities;
 interface Movable {
-    void move();
-    void setVelocity(int x, int y);
-    int getVelocityX();
-    int getVelocityY();
 }
