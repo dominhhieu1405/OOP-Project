3649a70230083b7abcfd664061d43ac6f7b7ca37
HuyPenguinn <24021519@vnu.edu.vn>
2025-10-01 15:54:52 +0700

    entity rework

--- src/entities/Ball.java
+++ src/entities/Ball.java
@@ -1,4 +1,8 @@
 package entities;
 import utils.Constants;
+import assets.Sprite;
 public class Ball extends Entity implements Movable {
+    public Ball(int x, int y) {
+        super(x, y, Constants.BALL_SPRITE);
+    }
 }
--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,4 +1,19 @@
 package entities;
+import assets.*;
 public abstract class Entity {
-    
+    protected int x;
+    protected int y;
+    protected Sprite sprite;
+
+    public Entity(int x, int y, Sprite sprite) {
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+    }
+    public int getX() {
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
 }
