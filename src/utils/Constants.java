cd34983cd582295138ae20e56d01077f9670fbca
HuyPenguinn <24021519@vnu.edu.vn>
2025-10-01 10:44:12 +0700

    update const

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -7,5 +7,8 @@ public class Constants {
     public static final int WINDOW_HEIGHT = 800;
     public static final int FPS = 60;
     public static final String GAME_TITLE = "Arkanoid";
-    
+    public static final int BRICK_WIDTH = 100;
+    public static final int BRICK_HEIGHT = 30;
+    public static final int PADDLE_WIDTH = 120;
+    public static final int PADDLE_HEIGHT = 20;
 }
